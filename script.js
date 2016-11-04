ac91477db44549f395dcab692c2e08b188d9c8ea
Sai Vadlamudi <saivadlamudi@hotmail.com>
2016-11-04 11:22:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ var answerArrayD = ['Does nothing', 'Bit', 'Google Company', 'Helpful Transactio
 var rightAnswers = ['Doubles', 'Protocol', 'Harvard College', 'Hypertext Transfer Protocol', 'Encode text in an easy to understand format', 'High Bandwidth; Low Latency', 'Internet Browser', 'Commercial', 'Client', 'Public',
                        '!DOCTYPE html', 'Brackets, Sublime', 'Comic Sans MS', 'li', 'script src="myscripts.js"', 'tr', 'img src="dankmemes.jpg"', 'hr', 'h1', 'Both of these answers are correct',
                        'h1 {color:red;}', 'h1 {padding:5px;}', 'p {margin: 100px 150px 100px 80px;}', '#', 'Margin', 'border-left: 6px solid red;', 'font-family: "Comic Sans MS";', 'a:visited', 'background-color: white;', 'text-align: left;',
-                       'console.log("Text")', '//Comment', 'textExample', 'Storing and Manipulating Text', 'innerHTML', 'var dankmemes = [Pepe, Dat Boi, Harambe]', 'ECMAScript', 'alert(“Hi There”);', 'if (i == x) then', 'function completeMath(x, y) {x*y}']
+                       'console.log("Text")', '//Comment', 'textExample', 'Storing and Manipulating Text', 'innerHTML', 'var dankmemes = [Pepe, Dat Boi, Harambe]', 'ECMAScript', 'alert("Hi There");', 'if (i == x) then', 'function completeMath(x, y) {x*y}']
 
 function random() { x = Math.random(); }
 
